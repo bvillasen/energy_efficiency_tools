d03a82b54eb41fcc0e47ad238f388b1a49887b89
bruno villasenor <bvillasen@gmail.com>
2025-07-09 22:06:40 -0700

    moved the frequency cap setting command inside the app_launcher

--- tools/app_launcher.sh
+++ tools/app_launcher.sh
@@ -18,6 +18,23 @@ fi
 # Get the hostname
 HOSTNAME=$(hostname)
 
+# List of devices to set the frequency cap
+DEVICE_IDS=(0 1 2 3 4 5 6 7)
+
+if [ -n "$FREQUENCY_CAP" ] && [ "$RANK_IS_ROOT" == "1" ]; then
+  # Set the frequency cap on all devices on the node
+  for DEVICE_ID in "${DEVICE_IDS[@]}"
+  do
+    NODE_ID=$(expr ${RANK_ID_GLOBAL} / ${N_RANKS_LOCAL})
+    set_freq_cmnd="/usr/bin/set_gpu_max_sclk -g ${DEVICE_ID} ${FREQUENCY_CAP}"
+    echo -e " Node_id: ${NODE_ID}  ${set_freq_cmnd} "
+    eval ${set_freq_cmnd} 
+  done
+
+  # Print the GPU frequency range
+  # rocm-smi --showsclkrange
+fi
+
 if [ "$RANK_IS_ROOT" == "1" ]; then
   ENERGY_COUNTER_FILE=${ENERGY_COUNTER_DIR}/energy_start_node_${NODE_ID}${ENERGY_COUNTER_SUFFIX}.txt
   echo "Collecting energy counter start. $(date)"
