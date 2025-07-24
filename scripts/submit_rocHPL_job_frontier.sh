7173f224168f4fc880654fdc2bd4adca3e044490
bruno villasenor <bvillasen@gmail.com>
2025-07-24 15:52:45 -0700

    Update submit_rocHPL_job_frontier.sh

--- scripts/submit_rocHPL_job_frontier.sh
+++ scripts/submit_rocHPL_job_frontier.sh
@@ -70,8 +70,11 @@ HPL_NB=512
 HPL_f=0.3
 N_ITER=1
 export OMP_NUM_THREADS=${N_CORES}
-export RUN_DIR=${WORK_DIR}
-export ENERGY_COUNTER_DIR=${WORK_DIR}
+export RUN_DIR=${WORK_DIR}/rocHPL
+if [ ! -d "${RUN_DIR}" ]; then
+  mkdir -p ${RUN_DIR}
+fi
+export ENERGY_COUNTER_DIR=${RUN_DIR}
 
 echo -e "Using rocHPL configuration for N_MPI=${N_MPI}"
 echo -e "HPL MPI grid N rows [P]: ${HPL_P}"
@@ -90,18 +93,24 @@ echo "ROCHPL_CMD: ${ROCHPL_CMD}"
 ${OMNISTAT_WRAPPER} usermode --start --interval ${SAMPLING_INTERVAL}
 
 # Run the application
-echo "Starting application. $(date)"
+echo "Starting rocHPL. $(date)"
 echo $(date +"%Y-%m-%d %H:%M:%S.%N") > ${RUN_DIR}/time_start.txt
 
 srun -n ${N_MPI} ${AFFINITY} $EE_TOOLS_PATH/tools/app_launcher.sh $ROCHPL_CMD > ${RUN_DIR}/app_output.txt
 
 echo $(date +"%Y-%m-%d %H:%M:%S.%N") > ${RUN_DIR}/time_end.txt
-echo "Finished application. $(date)"
+echo "Finished rocHPL. $(date)"
 
 # Finish Omnistat, generate summary pdf and copy database
 ${OMNISTAT_WRAPPER} usermode --stopexporters
 ${OMNISTAT_WRAPPER} query --interval ${SAMPLING_INTERVAL} --job ${SLURM_JOB_ID} --pdf ${RUN_DIR}/omnistat.${SLURM_JOB_ID}.pdf --export ${RUN_DIR}
 ${OMNISTAT_WRAPPER} usermode --stopserver
+mv /tmp/omnistat/${SLURM_JOB_ID} ${RUN_DIR}/data_omnistat.${SLURM_JOB_ID}
+mv exporter.log vic_server.log ${RUN_DIR}
+
+
+
+
 
 
 echo "Finished SLURM job. $(date)"
